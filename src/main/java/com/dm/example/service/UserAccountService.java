aa0c1990d58e0f1ff151942c0bccb38029d6ef97
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-06-10 15:55:24 +0800

    Update UserAccountService.java
    返回值为Json

--- src/main/java/com/dm/example/service/UserAccountService.java
+++ src/main/java/com/dm/example/service/UserAccountService.java
@@ -1,10 +1,9 @@
 package com.dm.example.service;
 
 import com.dm.example.beans.UserAccountBean;
-import com.dm.example.dto.ResultDto;
-import com.dm.example.exception.CustomException;
+
 
 public interface UserAccountService {
     //更新账号
-    ResultDto save(UserAccountBean paramBean);
+    String save(UserAccountBean paramBean);
 }
