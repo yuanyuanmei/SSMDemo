6faabac30a205547d6020beac270312bc4a92253
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-06-10 15:51:22 +0800

    Create ApiCodeConsts.java
    添加返回值常量

---
+++ src/main/java/com/dm/example/constants/ApiCodeConsts.java
@@ -0,0 +1,11 @@
+package com.dm.example.constants;
+
+//状态码
+public class ApiCodeConsts {
+
+    //成功
+    public static final int success = 200;
+
+    //失败
+    public static final int fail = 204;
+}
